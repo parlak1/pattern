e8b09947f3ea02578a0efc4ba18b62a214db1698
parlak1 <1purluck@gmail.com>
2023-06-02 11:57:08 -0400

    includes temporary js learning file

---
+++ pattern/perfectpita/perfectpita-web/src/js_learning.ts
@@ -0,0 +1,43 @@
+    type ObjeTipim = {
+        ilk: number,
+        ikinci: number
+    }
+
+    function returnSingleValue(inputA: number, inputB: number) { // 3, 5
+        return inputA + inputB
+    } // 8
+
+    function returnMultiValue(inputA: number, inputB: number): ObjeTipim { // 3, 5
+        return {"ilk":inputA*2, "ikinci":inputB*3}
+        // return [inputA*2, inputB*3]
+        
+    } // [6, 15]
+    // {"ilk":6, "ikici":15}
+
+    let a = {} // obje
+    let b = [] // array
+    let c = () => {} // function
+
+    let d:string
+    let e:number
+    let f:boolean
+    let g:null
+    let h:undefined
+    let i:unknown
+    let j:any
+    let k:void
+    let l:never
+    let m:object
+
+    const mySuperCallerFunction = () =>{
+        let acabaNe: ObjeTipim
+        // acabaNe = returnSingleValue(2, 3) // 5
+        acabaNe = returnMultiValue(4, 5) // {"ilk": 8, "ikinci": 15}
+
+        let {ilk, ikinci} = acabaNe
+
+        // let mySyperArr = ['apple', 'orange']
+        let[ilkim, ikincim] = ['apple', 'orange'] 
+        // ilkim = 'apple'
+        // ikincim = 'orange'
+    }
